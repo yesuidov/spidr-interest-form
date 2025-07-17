7429f92e4b9f010efff38d1949752bda52427fc4
Yesui Dovchinsuren <ydovc2@illinois.edu>
2025-07-16 21:59:57 -0500

    Add Spidr logo on the background

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import InterestForm from './components/InterestForm'
+import cornerImage from './assets/spidr-logo.png';
+import './App.css';
 
 function App() {
   return (
     <div className='form-wrapper'>
+      <img src={cornerImage} alt="" className="corner-image-outside" />
       <InterestForm />
     </div>
   )
